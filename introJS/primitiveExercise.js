7ff8f33c7187086f46e741274fa30e42281d7fd9
T-Wesst <code.daddy17@gmail.com>
2018-10-15 23:13:21 -0700

    complete primitve exercise

---
+++ introJS/primitiveExercise.js
@@ -0,0 +1,17 @@
+/*
+1.
+100 % 3
+100 / 3 = 33 
+33 * 3 == 99
+100 / 99 = remainder of 1
+
+ 2.
+("blah" + "blah")[6]
+"blahblah"[6]
+[6] = "a"
+
+3.
+"hello".length % "hi\\".length
+5 % 3
+5 / 3 = 1.66 remainer of 2
+*/
