24012cc47af8ea7c284829634d59b97719091403
T-Wesst <code.daddy17@gmail.com>
2018-10-15 23:25:06 -0700

    complete stalker exercise

---
+++ introJS/stalkerExercise.js
@@ -0,0 +1,16 @@
+/* JS Stalker Exercise
+variables, strings, prompt and console.console.log
+- ask for users first name
+- ask for users last name
+- ask for users age
+- print out users full name in a sentence
+- print out user age in a sentence
+*/
+
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+var fullName = firstName + " " + lastName;
+
+console.log("Hello, your full name is " + fullName);
+console.log("Your age is " + age);
