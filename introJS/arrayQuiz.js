143199e7b73be1004179b19346515153893fa361
T-Wesst <code.daddy17@gmail.com>
2018-10-25 22:50:11 -0700

    complete array quiz

---
+++ introJS/arrayQuiz.js
@@ -0,0 +1,14 @@
+// exercise 1.
+
+var numbers = [22, 67, 33, 96, 88];
+console.log(numbers[numbers.length]);
+// prints undefined
+
+// exercise 2.
+var friendGroups = [
+  ["harry", "ron", "hermione"],
+  ["malfroy", "crabbie", "goyle"],
+  ["mooney", "wormtail", "prongs"]
+];
+console.log(friendGroups[2][0]);
+// prints mooney
